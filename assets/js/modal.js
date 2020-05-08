8f82d59d55f8bb43604d596bec9bf46700308e6f
Vladimir <vgutman95@mail.ru>
2020-05-08 15:24:19 +0600

    Модальное окно на страницу lend

---
+++ assets/js/modal.js
@@ -0,0 +1,34 @@
+window.addEventListener('DOMContentLoaded' , function () {
+    let wrapBtn = document.querySelectorAll('.modal_wrap'),
+        btnOne = document.querySelectorAll('.modal_one'),
+        btnTwo = document.querySelectorAll('.modal_two'),
+        modalLend = document.querySelector('#exampleModalCenter1'),
+        modalBorrow = document.querySelector('#exampleModalCenter2'),
+        closeBtn = document.querySelectorAll('.modal-body');
+    
+        wrapBtn.forEach(function (i) {
+            i.addEventListener('click' , function (event) {
+                let target = event.target;
+                if(target && target.classList.contains('modal_one')){
+                    modalLend.classList.add('show');
+
+                    document.body.style.overflow = "hidden";
+                }
+                if(target && target.classList.contains('modal_two')){
+                    modalBorrow.classList.add('show');
+
+                    document.body.style.overflow = "hidden";
+                }
+            })
+        })
+        closeBtn.forEach(function (i) {
+            i.addEventListener('click', function (event) {
+               let target = event.target;
+               if(target && target.classList.contains('close')){
+                   modalLend.classList.remove('show');
+                   modalBorrow.classList.remove('show');
+                   document.body.style.overflow = "";
+               }
+            })
+        })
+})
